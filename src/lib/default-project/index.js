880349ce1c347750216d35bbeabaecb57a935d35
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-07-04 09:37:57 +0100

    Refactor defaultProject function for improved data handling
    Refactor defaultProject function to use translator and return projectJson data.

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -1,25 +1,46 @@
+import projectData from './project-data';
+
 /* eslint-disable import/no-unresolved */
 import overrideDefaultProject from '!arraybuffer-loader!./override-default-project.sb3';
+import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
+import costume1 from '!raw-loader!./dango-cat.svg';
 /* eslint-enable import/no-unresolved */
+import {TextEncoder} from '../tw-text-encoder';
 
-const defaultProject = () => {
-    // If your custom sb3 exists and is not empty, load it directly
-    if (overrideDefaultProject && overrideDefaultProject.byteLength > 0) {
+const defaultProject = translator => {
+    if (overrideDefaultProject.byteLength > 0) {
         return [{
             id: 0,
             assetType: 'Project',
-            // Do not use 'JSON' here if it is a full .sb3 zip file, 
-            // the vm loader will handle the binary array buffer automatically
+            dataFormat: 'JSON',
             data: overrideDefaultProject
         }];
     }
 
-    // Fallback empty project safety return if the file isn't found
+    let _TextEncoder;
+    if (typeof TextEncoder === 'undefined') {
+        _TextEncoder = require('text-encoding').TextEncoder;
+    } else {
+        _TextEncoder = TextEncoder;
+    }
+    const encoder = new _TextEncoder();
+
+    const projectJson = projectData(translator);
     return [{
         id: 0,
         assetType: 'Project',
         dataFormat: 'JSON',
-        data: JSON.stringify({targets: []})
+        data: JSON.stringify(projectJson)
+    }, {
+        id: 'cd21514d0531fdffb22204e0ec5ed84a',
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(backdrop)
+    }, {
+        id: '927d672925e7b99f7813735c484c6922',
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(costume1)
     }];
 };
 
