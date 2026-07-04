1d55504fe0941f178f61f51479177eebf518eef7
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-07-04 07:10:46 +0100

    Update costume imports and add new costumes

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -3,7 +3,10 @@ import projectData from './project-data';
 /* eslint-disable import/no-unresolved */
 import overrideDefaultProject from '!arraybuffer-loader!./override-default-project.sb3';
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
-import costume1 from '!raw-loader!./dango-cat.svg';
+import costume1 from '!raw-loader!./dango.svg';
+import costume2 from '!raw-loader!./build.svg';
+import costume3 from '!raw-loader!./phone.svg';
+import costume4 from '!raw-loader!./confused.svg';
 /* eslint-enable import/no-unresolved */
 import {TextEncoder} from '../tw-text-encoder';
 
@@ -41,6 +44,21 @@ const defaultProject = translator => {
         assetType: 'ImageVector',
         dataFormat: 'SVG',
         data: encoder.encode(costume1)
+    }, {
+        id: 'b3cd1258812e557b4b1a4390fb2031a5', // Placeholder ID for costume2
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(costume2)
+    }, {
+        id: 'c4de2369923f668c5c2b54a1fc3142b6', // Placeholder ID for costume3
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(costume3)
+    }, {
+        id: 'd5ef3470034a779d6d3c65b2fd4253c7', // Placeholder ID for costume4
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(costume4)
     }];
 };
 
