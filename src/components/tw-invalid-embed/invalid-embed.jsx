fe029a3df2ee119c21b41781116e580a811a7036
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-07-04 13:02:59 +0100

    Update link in invalid-embed component

--- src/components/tw-invalid-embed/invalid-embed.jsx
+++ src/components/tw-invalid-embed/invalid-embed.jsx
@@ -35,7 +35,7 @@ const InvalidEmbed = () => (
         <p>
             {'If you are seeing this page even though you aren\'t embedding anything, let us know on '}
             <a
-                href="https://scratch.mit.edu/users/GarboMuffin/#comments"
+                href="https://scratch.mit.edu/users/Noahscratch493/#comments"
                 target="_blank"
                 rel="noreferrer"
             >
