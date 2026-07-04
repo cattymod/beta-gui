b3e316b5466707eb6de57f9e434530969265f02f
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-07-04 10:15:02 +0100

    Update render-interface.jsx

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -341,7 +341,7 @@ class Interface extends React.Component {
                                 </p>
                             </div>
                             <div className={styles.section}>
-                                <FeaturedProjects studio="27205657" />
+                                <FeaturedProjects studio="51500179" />
                             </div>
                         </React.Fragment>
                     ) : null}
