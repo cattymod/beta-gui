504f2fa451cef046c1ad9fb19b4959b951769311
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-07-05 10:31:59 +0100

    Update settings.jsx

--- src/addons/settings/settings.jsx
+++ src/addons/settings/settings.jsx
@@ -1104,7 +1104,7 @@ class AddonSettingsComponent extends React.Component {
                             />
                         </div>
                         <a
-                            href="https://scratch.mit.edu/users/Noahscratch493/#comments"
+                            href="https://scratch.mit.edu/projects/1335032585/"
                             target="_blank"
                             rel="noreferrer"
                             className={styles.feedbackButtonOuter}
