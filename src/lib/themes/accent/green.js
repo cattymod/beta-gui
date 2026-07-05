8809d53f3f0a36f5b4573eae60e9e4fcd6cf7d32
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-07-05 13:27:07 +0100

    Add green theme color definitions

---
+++ src/lib/themes/accent/green.js
@@ -0,0 +1,13 @@
+const guiColors = {
+    'looks-secondary': 'hsla(125, 85%, 38%, 1)',
+    'looks-transparent': 'hsla(125, 85%, 38%, 0.35)',
+    'looks-light-transparent': 'hsla(125, 85%, 38%, 0.15)',
+    'looks-secondary-dark': 'hsla(125, 80%, 25%, 1)'
+};
+
+const blockColors = {};
+
+export {
+    guiColors,
+    blockColors
+};
