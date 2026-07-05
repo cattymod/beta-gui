12dae6b29bb870492e7f740a7d0d7e7a9457dedc
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-07-05 07:12:44 +0100

    Delete src/addons/addons/tab-styles directory

--- src/addons/addons/tab-styles/_runtime_entry.js
+++
@@ -1,7 +0,0 @@
-import _turbowarpCSS from "!css-loader!./turbowarp.css";
-import _scratchboxCSS from "!css-loader!./scratchbox.css";
-
-export const resources = {
-    "turbowarp.css": _turbowarpCSS,
-    "scratchbox.css": _scratchboxCSS,
-};
