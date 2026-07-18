535bbdc8ffb7bb48b812f92a210e2b61bcaa33bf
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-07-18 16:16:47 +0100

    Update hash-parser-hoc.jsx

--- src/lib/hash-parser-hoc.jsx
+++ src/lib/hash-parser-hoc.jsx
@@ -4,17 +4,12 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
 import {
+    defaultProjectId,
     getIsFetchingWithoutId,
     setProjectId
 } from '../reducers/project-state';
 
-/*
- * Higher Order Component to get the project id from location.hash
- * Supports:
- * /
- * /editor#PROJECT_ID
- * /fullscreen#PROJECT_ID
- *
+/* Higher Order Component to get the project id from location.hash
  * @param {React.Component} WrappedComponent: component to render
  * @returns {React.Component} component with hash parsing behavior
  */
@@ -22,82 +17,30 @@ const HashParserHOC = function (WrappedComponent) {
     class HashParserComponent extends React.Component {
         constructor (props) {
             super(props);
-
             bindAll(this, [
-                'handleHashChange',
-                'handlePathChange'
+                'handleHashChange'
             ]);
         }
-
         componentDidMount () {
             window.addEventListener('hashchange', this.handleHashChange);
-            window.addEventListener('popstate', this.handlePathChange);
-
-            this.handlePathChange();
+            this.handleHashChange();
         }
-
         componentDidUpdate (prevProps) {
-            // If we are newly fetching a non-hash project,
-            // remove the hash instead of creating #0
+            // if we are newly fetching a non-hash project...
             if (this.props.isFetchingWithoutId && !prevProps.isFetchingWithoutId) {
-                history.replaceState(
-                    'new-project',
-                    'new-project',
-                    window.location.pathname + window.location.search
-                );
-            }
-
-            // Keep URL hash synced with project ID
-            if (this.props.reduxProjectId !== prevProps.reduxProjectId) {
-                const id = this.props.reduxProjectId;
-
-                if (id && id !== '0') {
-                    if (window.location.hash !== `#${id}`) {
-                        history.replaceState(
-                            null,
-                            '',
-                            `${window.location.pathname}${window.location.search}#${id}`
-                        );
-                    }
-                } else if (window.location.hash === '#0') {
-                    history.replaceState(
-                        null,
-                        '',
-                        window.location.pathname + window.location.search
-                    );
-                }
+                // ...clear the hash from the url
+                history.pushState('new-project', 'new-project',
+                    window.location.pathname + window.location.search);
             }
         }
-
         componentWillUnmount () {
             window.removeEventListener('hashchange', this.handleHashChange);
-            window.removeEventListener('popstate', this.handlePathChange);
         }
-
-        handlePathChange () {
-            const path = window.location.pathname;
-
-            // Allow only these routes
-            if (
-                path === '/' ||
-                path === '/editor' ||
-                path === '/fullscreen'
-            ) {
-                this.handleHashChange();
-            }
-        }
-
         handleHashChange () {
             const hashMatch = window.location.hash.match(/#(\d+)/);
-
-            if (!hashMatch || hashMatch[1] === '0') {
-                this.props.setProjectId(null);
-                return;
-            }
-
-            this.props.setProjectId(hashMatch[1].toString());
+            const hashProjectId = hashMatch === null ? defaultProjectId : hashMatch[1];
+            this.props.setProjectId(hashProjectId.toString());
         }
-
         render () {
             const {
                 /* eslint-disable no-unused-vars */
@@ -107,7 +50,6 @@ const HashParserHOC = function (WrappedComponent) {
                 /* eslint-enable no-unused-vars */
                 ...componentProps
             } = this.props;
-
             return (
                 <WrappedComponent
                     {...componentProps}
@@ -115,38 +57,27 @@ const HashParserHOC = function (WrappedComponent) {
             );
         }
     }
-
     HashParserComponent.propTypes = {
         isFetchingWithoutId: PropTypes.bool,
-        reduxProjectId: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.number
-        ]),
+        reduxProjectId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
         setProjectId: PropTypes.func
     };
-
     const mapStateToProps = state => {
         const loadingState = state.scratchGui.projectState.loadingState;
-
         return {
             isFetchingWithoutId: getIsFetchingWithoutId(loadingState),
             reduxProjectId: state.scratchGui.projectState.projectId
         };
     };
-
     const mapDispatchToProps = dispatch => ({
         setProjectId: projectId => {
             dispatch(setProjectId(projectId));
         }
     });
-
+    // Allow incoming props to override redux-provided props. Used to mock in tests.
     const mergeProps = (stateProps, dispatchProps, ownProps) => Object.assign(
-        {},
-        stateProps,
-        dispatchProps,
-        ownProps
+        {}, stateProps, dispatchProps, ownProps
     );
-
     return connect(
         mapStateToProps,
         mapDispatchToProps,
