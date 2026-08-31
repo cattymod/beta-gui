5bfd2cbec8aa3d4d1d4157426cd6c33fbf6e7a76
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-08-31 07:40:04 +0100

    Reformat yellow.js to match red.js structure with yellow colors

--- src/lib/themes/accent/yellow.js
+++ src/lib/themes/accent/yellow.js
@@ -1,11 +1,25 @@
 const guiColors = {
-    'looks-secondary': 'hsla(52, 100%, 45%, 1)',
-    'looks-transparent': 'hsla(52, 100%, 45%, 0.35)',
-    'looks-light-transparent': 'hsla(52, 100%, 45%, 0.15)',
-    'looks-secondary-dark': 'hsla(52, 90%, 30%, 1)'
+    'motion-primary': '#f1c40f',
+    'motion-primary-transparent': '#f1c40fe6',
+    'motion-tertiary': '#c1a000',
+
+    'looks-secondary': '#f1c40f',
+    'looks-transparent': '#f1c40f59',
+    'looks-light-transparent': '#f1c40f26',
+    'looks-secondary-dark': 'hsla(48, 98%, 47%, 1)',
+
+    'extensions-primary': 'hsla(48, 100%, 55%, 1)',
+    'extensions-tertiary': 'hsla(48, 100%, 40%, 1)',
+    'extensions-transparent': 'hsla(48, 100%, 55%, 0.35)',
+    'extensions-light': 'hsla(48, 57%, 85%, 1)',
+
+    'drop-highlight': '#f9e79f'
 };
 
-const blockColors = {};
+const blockColors = {
+    checkboxActiveBackground: '#f1c40f',
+    checkboxActiveBorder: '#c1a000'
+};
 
 export {
     guiColors,
