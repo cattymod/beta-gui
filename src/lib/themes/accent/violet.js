5a93a89db8d39a35f96914dfcf5e8ab234974eeb
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-08-31 07:39:25 +0100

    Reformat violet.js to match red.js structure with violet colors

--- src/lib/themes/accent/violet.js
+++ src/lib/themes/accent/violet.js
@@ -1,11 +1,25 @@
 const guiColors = {
-    'looks-secondary': 'hsla(280, 100%, 65%, 1)',
-    'looks-transparent': 'hsla(280, 100%, 65%, 0.35)',
-    'looks-light-transparent': 'hsla(280, 100%, 65%, 0.15)',
-    'looks-secondary-dark': 'hsla(280, 60%, 50%, 1)'
+    'motion-primary': '#9b59b6',
+    'motion-primary-transparent': '#9b59b6e6',
+    'motion-tertiary': '#7d3c98',
+
+    'looks-secondary': '#9b59b6',
+    'looks-transparent': '#9b59b659',
+    'looks-light-transparent': '#9b59b626',
+    'looks-secondary-dark': 'hsla(283, 53%, 51%, 1)',
+
+    'extensions-primary': 'hsla(283, 85%, 65%, 1)',
+    'extensions-tertiary': 'hsla(283, 85%, 40%, 1)',
+    'extensions-transparent': 'hsla(283, 85%, 65%, 0.35)',
+    'extensions-light': 'hsla(283, 57%, 85%, 1)',
+
+    'drop-highlight': '#c788d9'
 };
 
-const blockColors = {};
+const blockColors = {
+    checkboxActiveBackground: '#9b59b6',
+    checkboxActiveBorder: '#7d3c98'
+};
 
 export {
     guiColors,
