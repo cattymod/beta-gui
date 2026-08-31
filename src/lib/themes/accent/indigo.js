9e5d0399a23671f53290725d4e83a561888dc521
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-08-31 07:39:45 +0100

    Reformat indigo.js to match red.js structure with indigo colors

--- src/lib/themes/accent/indigo.js
+++ src/lib/themes/accent/indigo.js
@@ -1,11 +1,25 @@
 const guiColors = {
-    'looks-secondary': 'hsla(250, 100%, 65%, 1)',
-    'looks-transparent': 'hsla(250, 100%, 65%, 0.35)',
-    'looks-light-transparent': 'hsla(250, 100%, 65%, 0.15)',
-    'looks-secondary-dark': 'hsla(250, 60%, 50%, 1)'
+    'motion-primary': '#5b5dff',
+    'motion-primary-transparent': '#5b5dffe6',
+    'motion-tertiary': '#4a4ccc',
+
+    'looks-secondary': '#5b5dff',
+    'looks-transparent': '#5b5dff59',
+    'looks-light-transparent': '#5b5dff26',
+    'looks-secondary-dark': 'hsla(238, 100%, 51%, 1)',
+
+    'extensions-primary': 'hsla(238, 100%, 65%, 1)',
+    'extensions-tertiary': 'hsla(238, 100%, 40%, 1)',
+    'extensions-transparent': 'hsla(238, 100%, 65%, 0.35)',
+    'extensions-light': 'hsla(238, 57%, 85%, 1)',
+
+    'drop-highlight': '#8a8cff'
 };
 
-const blockColors = {};
+const blockColors = {
+    checkboxActiveBackground: '#5b5dff',
+    checkboxActiveBorder: '#4a4ccc'
+};
 
 export {
     guiColors,
