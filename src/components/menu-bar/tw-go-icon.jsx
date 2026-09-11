830a065c3a8af8806559976b8485a7699413c422
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-09-11 17:17:16 +0100

    Update tw-go-icon.jsx

--- src/components/menu-bar/tw-go-icon.jsx
+++ src/components/menu-bar/tw-go-icon.jsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 
 import {MenuItem, Submenu} from '../menu/menu.jsx';
@@ -15,6 +15,12 @@ import {
     setGoIcon,
     applyGoIcon
 } from '../../lib/go-icon';
+
+import {
+    openGoIconMenu,
+    goIconMenuOpen,
+    closeSettingsMenu
+} from '../../reducers/menus.js';
 
 import styles from './settings-menu.css';
 
@@ -47,17 +53,12 @@ GoIconPreview.propTypes = {
     icon: PropTypes.string
 };
 
-const GoIconMenuItem = ({
-    icon,
-    isSelected,
-    label,
-    onClick
-}) => (
-    <MenuItem onClick={onClick}>
+const GoIconMenuItem = props => (
+    <MenuItem onClick={props.onClick}>
         <div className={styles.option}>
             <img
                 className={classNames(styles.check, {
-                    [styles.selected]: isSelected
+                    [styles.selected]: props.isSelected
                 })}
                 width={15}
                 height={12}
@@ -66,9 +67,9 @@ const GoIconMenuItem = ({
                 alt=""
             />
 
-            <GoIconPreview icon={icon} />
+            <GoIconPreview icon={props.icon} />
 
-            <span>{label}</span>
+            <span>{props.label}</span>
         </div>
     </MenuItem>
 );
@@ -83,73 +84,19 @@ GoIconMenuItem.propTypes = {
 const TWGoIcon = ({
     isOpen,
     isRtl,
-    onRequestClose,
-    onCloseOtherMenus
+    onChangeGoIcon,
+    onOpen,
+    goIcon
 }) => {
-    const [goIcon, setGoIconState] = useState(() =>
-        isFileProtocol() ? GO_ICON_PLAY : getGoIcon()
-    );
-
-    const [goIconMenuOpen, setGoIconMenuOpen] = useState(false);
-
-    useEffect(() => {
-        if (!isOpen) {
-            setGoIconMenuOpen(false);
-        }
-    }, [isOpen]);
-
-    useEffect(() => {
-        if (isFileProtocol()) {
-            setGoIconMenuOpen(false);
-            return undefined;
-        }
-
-        applyGoIcon(goIcon);
-
-        return undefined;
-    }, [goIcon]);
-
-    function onChangeGoIcon(mode) {
-        if (isFileProtocol()) {
-            return;
-        }
-
-        if (
-            mode !== GO_ICON_PLAY &&
-            mode !== GO_ICON_GREEN_FLAG &&
-            mode !== GO_ICON_BLUE_FLAG &&
-            mode !== GO_ICON_PURPLE_FLAG
-        ) {
-            return;
-        }
-
-        setGoIcon(mode);
-        setGoIconState(mode);
-        applyGoIcon(mode);
-
-        setGoIconMenuOpen(false);
-        onRequestClose();
-    }
-
-    function onOpenGoIconMenu() {
-        if (isFileProtocol()) {
-            return;
-        }
-
-        onCloseOtherMenus();
-
-        setGoIconMenuOpen(true);
-    }
-
     if (isFileProtocol()) {
         return null;
     }
 
     return (
-        <MenuItem expanded={goIconMenuOpen}>
+        <MenuItem expanded={isOpen}>
             <div
                 className={styles.option}
-                onClick={onOpenGoIconMenu}
+                onClick={onOpen}
             >
                 <GoIconPreview
                     icon={getGoIconImage(goIcon)}
@@ -172,28 +119,36 @@ const TWGoIcon = ({
                     icon={getGoIconImage(GO_ICON_PLAY)}
                     label="Play Button (default)"
                     isSelected={goIcon === GO_ICON_PLAY}
-                    onClick={() => onChangeGoIcon(GO_ICON_PLAY)}
+                    onClick={() =>
+                        onChangeGoIcon(GO_ICON_PLAY)
+                    }
                 />
 
                 <GoIconMenuItem
                     icon={getGoIconImage(GO_ICON_GREEN_FLAG)}
                     label="Green Flag"
                     isSelected={goIcon === GO_ICON_GREEN_FLAG}
-                    onClick={() => onChangeGoIcon(GO_ICON_GREEN_FLAG)}
+                    onClick={() =>
+                        onChangeGoIcon(GO_ICON_GREEN_FLAG)
+                    }
                 />
 
                 <GoIconMenuItem
                     icon={getGoIconImage(GO_ICON_BLUE_FLAG)}
                     label="Blue Flag"
                     isSelected={goIcon === GO_ICON_BLUE_FLAG}
-                    onClick={() => onChangeGoIcon(GO_ICON_BLUE_FLAG)}
+                    onClick={() =>
+                        onChangeGoIcon(GO_ICON_BLUE_FLAG)
+                    }
                 />
 
                 <GoIconMenuItem
                     icon={getGoIconImage(GO_ICON_PURPLE_FLAG)}
                     label="Purple Flag"
                     isSelected={goIcon === GO_ICON_PURPLE_FLAG}
-                    onClick={() => onChangeGoIcon(GO_ICON_PURPLE_FLAG)}
+                    onClick={() =>
+                        onChangeGoIcon(GO_ICON_PURPLE_FLAG)
+                    }
                 />
             </Submenu>
         </MenuItem>
@@ -203,8 +158,38 @@ const TWGoIcon = ({
 TWGoIcon.propTypes = {
     isOpen: PropTypes.bool,
     isRtl: PropTypes.bool,
-    onRequestClose: PropTypes.func,
-    onCloseOtherMenus: PropTypes.func
+    onChangeGoIcon: PropTypes.func,
+    onOpen: PropTypes.func,
+    goIcon: PropTypes.string
 };
 
-export default TWGoIcon;
+const mapStateToProps = state => ({
+    isOpen: goIconMenuOpen(state),
+    isRtl: state.locales.isRtl,
+    goIcon: getGoIcon()
+});
+
+const mapDispatchToProps = dispatch => ({
+    onChangeGoIcon: mode => {
+        if (
+            mode !== GO_ICON_PLAY &&
+            mode !== GO_ICON_GREEN_FLAG &&
+            mode !== GO_ICON_BLUE_FLAG &&
+            mode !== GO_ICON_PURPLE_FLAG
+        ) {
+            return;
+        }
+
+        setGoIcon(mode);
+        applyGoIcon(mode);
+
+        dispatch(closeSettingsMenu());
+    },
+
+    onOpen: () => dispatch(openGoIconMenu())
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(TWGoIcon);
