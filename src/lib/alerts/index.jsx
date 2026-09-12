1f31052ff78b52137db50add03af04f0597b3234
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-09-12 06:39:47 +0100

    Update index.jsx

--- src/lib/alerts/index.jsx
+++ src/lib/alerts/index.jsx
@@ -163,7 +163,7 @@ const alerts = [
             'twRestorePointSuccess', 'twRestorePointError'],
         content: (
             <FormattedMessage
-                defaultMessage="Saved to your device."
+                defaultMessage="Saved to your computer."
                 description="Message indicating that project was successfully saved to the user's disk"
                 id="tw.alerts.savedToDisk"
             />
