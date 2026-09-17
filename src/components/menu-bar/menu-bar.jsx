dc16335b0518bb30a65082a7706a14ceba46a482
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-09-17 07:20:23 +0100

    Update menu-bar.jsx

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -231,7 +231,8 @@ class MenuBar extends React.Component {
             'handleKeyPress',
             'handleRestoreOption',
             'getSaveToComputerHandler',
-            'restoreOptionMessage'
+            'restoreOptionMessage',
+            'handleClickGoHome'
         ]);
     }
 
@@ -310,8 +311,12 @@ class MenuBar extends React.Component {
     }
 
     handleClickRestorePoints () {
-        this.props.onClickRestorePoints();
-        this.props.onRequestCloseFile();
+    this.props.onClickRestorePoints();
+    this.props.onRequestCloseFile();
+}
+
+    handleClickGoHome () {
+        window.location.href = 'https://cattymod.app';
     }
 
     handleClickSeeCommunity (waitForUpdate) {
@@ -1005,33 +1010,61 @@ class MenuBar extends React.Component {
                                             </MenuItem>
                                         </MenuSection>
                                     )}
+<MenuSection>
+    <MenuItem
+        onClick={this.handleClickRestorePoints}
+    >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8" />
+            <path d="M21 3v5h-5" />
+        </svg>
 
-                                    <MenuSection>
-                                        <MenuItem
-                                            onClick={this.handleClickRestorePoints}
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="24"
-                                                height="24"
-                                                viewBox="0 0 24 24"
-                                                fill="none"
-                                                stroke="currentColor"
-                                                strokeWidth="2"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                            >
-                                                <path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8" />
-                                                <path d="M21 3v5h-5" />
-                                            </svg>
+        <FormattedMessage
+            defaultMessage="Restore points"
+            description="Menu bar item to manage restore points"
+            id="tw.menuBar.restorePoints"
+        />
+    </MenuItem>
 
-                                            <FormattedMessage
-                                                defaultMessage="Restore points"
-                                                description="Menu bar item to manage restore points"
-                                                id="tw.menuBar.restorePoints"
-                                            />
-                                        </MenuItem>
-                                    </MenuSection>
+    {!isEmbeddedEditor && (
+        <MenuItem
+            isRtl={this.props.isRtl}
+            onClick={this.handleClickGoHome}
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-house"
+            >
+                <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+                <path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+            </svg>
+
+            <FormattedMessage
+                defaultMessage="Go Home"
+                description="Menu bar item for returning to the CattyMod home page"
+                id="tw.menuBar.goHome"
+            />
+        </MenuItem>
+    )}
+</MenuSection>
 
                                 </MenuBarMenu>
                             </MenuLabel>
