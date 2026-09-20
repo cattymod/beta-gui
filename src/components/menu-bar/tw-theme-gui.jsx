64a5f10e4902247e8d28849f23754d61e13b0f2e
Noahscratch493 <161503774+Noahscratch493@users.noreply.github.com>
2026-09-20 12:00:33 +0100

    Update tw-theme-gui.jsx

--- src/components/menu-bar/tw-theme-gui.jsx
+++ src/components/menu-bar/tw-theme-gui.jsx
@@ -76,25 +76,30 @@ const ThemeIcon = ({theme}) => {
         );
     }
 
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className={styles.themeIcon}
-            aria-hidden="true"
-        >
-            <path d="M18 5h4"/>
-            <path d="M20 3v4"/>
-            <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/>
-        </svg>
-    );
+    // Midnight theme
+    if (theme === GUI_MIDNIGHT) {
+        return (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className={styles.themeIcon}
+                aria-hidden="true"
+            >
+                <path d="M18 5h4"/>
+                <path d="M20 3v4"/>
+                <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/>
+            </svg>
+        );
+    }
+
+    return null;
 };
 
 ThemeIcon.propTypes = {
@@ -147,6 +152,9 @@ const GuiThemeMenu = ({
             // eslint-disable-next-line react/jsx-no-bind
             onClick={onOpen}
         >
+            {/* Show the currently selected theme icon on the main Theme button */}
+            <ThemeIcon theme={theme.gui}/>
+
             <span className={styles.submenuLabel}>
                 <FormattedMessage
                     defaultMessage="Theme"
